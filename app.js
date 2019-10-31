800e3ceb0043eca9be56a7475ec1ce5dbc9f31c0
Fernando <f.sanchezvilela@gmail.com>
2019-10-31 22:36:23 +0100

    add: LocalStorage save data and Change Weather method

--- app.js
+++ app.js
@@ -1,11 +1,32 @@
+// Init Storage
+const storage = new Storage();
+//Get Stored location data
+const weatherLocation = storage.getLocationData();
 // Init Weather Object
-const weather = new Weather('Boston', 'us');
+const weather = new Weather(weatherLocation.city, weatherLocation.country);
 //Init UI
 const ui = new UI();
 // Get Weather on DOM load
 document.addEventListener('DOMContentLoaded', getWeather);
 
-// weather.changeLocation('Miami', 'us')
+//Change location Event
+document.getElementById('w-change-btn').addEventListener('click',(e)=>{
+  const city = document.getElementById('city').value;
+  const country = document.getElementById('country').value;
+
+  // Change Location
+  weather.changeLocation(city,country)
+
+  // Set Location in LS
+  storage.setLocationData(city,country);
+
+  // Get and Display weather
+  getWeather();
+  // Close modal
+  $('#locModal').modal('hide');
+})
+
+
 
 
 function getWeather() {
